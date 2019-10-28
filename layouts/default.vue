11b5554566883103e0bf2812e0b1826d59ed9e0b
marco <28888207+Marco3jp@users.noreply.github.com>
2019-10-29 04:32:23 +0900

    デフォルトのUIを少し改変しました

---
+++ layouts/default.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-app dark class="custom-font">
+    <v-toolbar max-height="64">
+      <v-toolbar-title>
+        <nuxt-link to="/" class="unset-link-color">{{this.title}}</nuxt-link>
+      </v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-toolbar-items>
+        <v-btn text>ItemA</v-btn>
+        <v-btn text>ItemB</v-btn>
+        <v-btn text>ItemC</v-btn>
+      </v-toolbar-items>
+
+      <template>
+        <v-btn icon>
+          <v-icon>mdi-export-variant</v-icon>
+        </v-btn>
+        <v-btn icon>
+          <v-icon>mdi-delete-circle</v-icon>
+        </v-btn>
+        <v-btn icon>
+          <v-icon>mdi-plus-circle</v-icon>
+        </v-btn>
+      </template>
+    </v-toolbar>
+
+    <v-content>
+      <v-container>
+        <nuxt/>
+      </v-container>
+    </v-content>
+
+    <v-footer app>
+      <span>&copy; 2019</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+  import { LightLife } from '../lib/lightLife.ts'
+
+  export default {
+    data() {
+      return {
+        title: 'us-life'
+      }
+    },
+    created() {
+      this.$store.commit('life/set', new LightLife())
+    }
+  }
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-layout class="justify-center">
+    <v-card outlined class="px-4 py-2">
+      <v-card-title class="display-1">
+        Welcome to the extraordinary-us-life.
+      </v-card-title>
+      <v-card-subtitle class="subtitle-1 text-right mr-3 blue-grey--text text--lighten-4">
+        非日常の中で、表現の力を信じた。
+      </v-card-subtitle>
+      <v-card-text class="body-1">
+        <p>これは私たちの普段の生活が非日常に変わる物語。</p>
+        <p>きっとそんなときに貫くであろう、<strong>"ものづくり"</strong>の精神を詰め込みました。</p>
+        <div class="text-xs-right">
+          <small>Author: Marco</small>
+        </div>
+        <hr class="my-3">
+        <p class="caption">この作品に登場する人物は実在せず、物語はフィクションです。</p>
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer/>
+        <v-btn color="primary" nuxt :to=playLink>
+          PLAY
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-layout>
+</template>
+
+<script>
+  export default {
+    computed: {
+      playLink() {
+        const completedInit = localStorage.getItem('completedInit')
+        if (completedInit !== null && completedInit === 'true') {
+          return '/loadSaveData'
+        } else {
+          return '/opening'
+        }
+      }
+    }
+  }
+</script>
