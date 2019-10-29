6e89e4b4dbfaf7038970d9433bcae403cf3b28c7
marco <28888207+Marco3jp@users.noreply.github.com>
2019-10-30 03:43:40 +0900

    ノベルパートで使う表示領域の行を実装

---
+++ components/consoleRow.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-card outlined>{{sentence.by}}> {{this.row}}</v-card>
+</template>
+
+<script>
+  export default {
+    name: 'consoleRow',
+    props: ['sentence', 'isCurrentRow'],
+    computed: {
+      row() {
+        if (this.isCurrentRow && !this.$store.state.sentence.isEndSection) {
+          return this.sentence.text.substring(0, this.$store.state.sentence.characterPosition)
+        } else {
+          return this.sentence.text
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
