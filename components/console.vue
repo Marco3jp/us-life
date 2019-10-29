98758f270e7fe80fcebb6cef3ec18ab7fc200878
marco <28888207+Marco3jp@users.noreply.github.com>
2019-10-30 03:43:57 +0900

    ノベルパートで使う表示領域を実装

---
+++ components/console.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-card raised height="600" width="1200">
+    <console-row v-for="(sentence, index) in scrollbackBuffer" :sentence="sentence" :key="index"
+                 :isCurrentRow="scrollbackBuffer.length-1 === index"></console-row>
+  </v-card>
+</template>
+
+<script>
+  import ConsoleRow from '~/components/consoleRow'
+
+  export default {
+    name: 'console',
+    components: { ConsoleRow },
+    computed: {
+      scrollbackBuffer() {
+        return this.$store.state.console.scrollback
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
