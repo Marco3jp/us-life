56a572ea29a214c5f49d6c1062dd5823c2f55d56
marco <28888207+Marco3jp@users.noreply.github.com>
2019-11-02 04:31:57 +0900

    ViewScriptの仕様に、表示されないことを示すsilentと、スタックされた何かが残っていることを示すtoBeContinueを追加

--- lib/model/viewScript.ts
+++ lib/model/viewScript.ts
@@ -6,6 +6,8 @@ import { ViewSection } from '~/lib/model/viewScript/viewSection'
  * childActionsはselectChildActionによってユーザーに提示される。
  */
 export interface ViewScript {
-  sections: Array<ViewSection>,
-  childActions?: Array<Action>
+  sections?: Array<ViewSection>,
+  childActions?: Array<Action>,
+  toBeContinue?: boolean
+  silent?: boolean
 }
