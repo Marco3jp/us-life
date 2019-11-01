d58658bacb74c93a61ea2a78175c452b455d0c52
marco <28888207+Marco3jp@users.noreply.github.com>
2019-11-02 00:07:20 +0900

    ロケーションリストを追加

--- lib/gameAssets/locationList.ts
+++ lib/gameAssets/locationList.ts
@@ -1,3 +1,23 @@
 import { Location as LocationModel } from '../model/location'
 
-export let LocationList: Array<LocationModel> = []
+export let LocationList: Array<LocationModel> = [
+  {
+    id: 0,
+    name: '工房'
+  }, {
+    id: 1,
+    name: '部室棟'
+  }, {
+    id: 2,
+    name: '球場'
+  }, {
+    id: 3,
+    name: '講義棟'
+  }, {
+    id: 4,
+    name: '学部棟'
+  }, {
+    id: 6,
+    name: '中庭'
+  }
+]
