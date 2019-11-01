ade8783478d221643a6dd588dce0b9ba7394c6ef
marco <28888207+Marco3jp@users.noreply.github.com>
2019-11-02 04:48:12 +0900

    イベントの発生についてTextは必須ではなかったので修正

--- lib/model/event.ts
+++ lib/model/event.ts
@@ -4,9 +4,9 @@ export interface Event {
   id: number
   name: string
   require: Function // if return true, you can use it.
-  requireText: string
+  requireText?: string
   effect: Function
-  effectText: string
+  effectText?: string
   weight?: number
   script?: ViewScript
 }
