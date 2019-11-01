75c3ffafb3d5a89fb3883e7b334fa89f3705bad4
marco <28888207+Marco3jp@users.noreply.github.com>
2019-11-02 00:03:57 +0900

    今回の実装に不要な箇所を削除

--- lib/model/location.ts
+++ lib/model/location.ts
@@ -5,10 +5,4 @@ import { ViewScript } from './viewScript'
 export interface Location {
   id: number
   name: string
-  require: Function
-  requireText: string
-  coordinate: Coordinate
-  area: Area
-  script?: ViewScript
-  childLocation?: Array<Location>
 }
