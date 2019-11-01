9875d33e2a18762be12518e88ee66c1dea7294b7
marco <28888207+Marco3jp@users.noreply.github.com>
2019-11-02 04:15:12 +0900

    getFlagが必ずBooleanを返すように修正、未定義のFlagで触るとconsole.warnを吐くようにした

--- lib/module/state/flag.ts
+++ lib/module/state/flag.ts
@@ -1,13 +1,17 @@
+import { flagList } from '../../gameAssets/flagList'
+
 export class Flag {
   flagStore: Object
+  definedFlags: Array<string>
 
   constructor() {
+    this.definedFlags = flagList
     this.loadFlags()
   }
 
   setFlag(key: string, value: boolean) {
-    if (typeof value !== 'boolean') {
-      throw new TypeError()
+    if (!this.definedFlags.includes(key)) {
+      this.warnUndefinedFlag(key)
     }
     this.flagStore[key] = value
     this.saveFlags()
@@ -15,6 +19,9 @@ export class Flag {
 
   removeFlag(key: string): boolean {
     if (typeof this.flagStore[key] !== 'undefined') {
+      if (!this.definedFlags.includes(key)) {
+        this.warnUndefinedFlag(key)
+      }
       const result = delete this.flagStore[key]
       this.saveFlags()
       return result
@@ -23,8 +30,11 @@ export class Flag {
     }
   }
 
-  getFlag(key: string): boolean | undefined {
-    return this.flagStore[key]
+  getFlag(key: string): boolean {
+    if (!this.definedFlags.includes(key)) {
+      this.warnUndefinedFlag(key)
+    }
+    return typeof this.flagStore[key] !== 'undefined' ? this.flagStore[key] : false
   }
 
   private static getStorageKey() {
@@ -42,5 +52,9 @@ export class Flag {
 
   private saveFlags() {
     localStorage.setItem(Flag.getStorageKey(), JSON.stringify(this.flagStore))
+  }
+
+  private warnUndefinedFlag(key: string) {
+    console.warn(`key: ${key} flag is not defined. If you are developer, check typo. else, please send this message to developer.`)
   }
 }
