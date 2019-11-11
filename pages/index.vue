d3f6492048f2838bee2adb0e3182c092575588e1
marco <28888207+Marco3jp@users.noreply.github.com>
2019-11-11 21:15:49 +0900

    トップページの体裁とリンクを修正

--- pages/index.vue
+++ pages/index.vue
@@ -4,14 +4,14 @@
       <v-card-title class="display-1">
         Welcome to the extraordinary-us-life.
       </v-card-title>
-      <v-card-subtitle class="subtitle-1 text-right mr-3 blue-grey--text text--lighten-4">
+      <v-card-subtitle class="headline text-right mr-3 blue-grey--text text--lighten-4">
         非日常の中で、表現の力を信じた。
       </v-card-subtitle>
-      <v-card-text class="body-1">
+      <v-card-text class="body-0">
         <p>これは私たちの普段の生活が非日常に変わる物語。</p>
         <p>きっとそんなときに貫くであろう、<strong>"ものづくり"</strong>の精神を詰め込みました。</p>
-        <div class="text-xs-right">
-          <small>Author: Marco</small>
+        <div class="body-2">
+          <p>Author: Marco</p>
         </div>
         <hr class="my-3">
         <p class="caption">この作品に登場する人物は実在せず、物語はフィクションです。</p>
@@ -33,12 +33,7 @@
   export default {
     computed: {
       playLink() {
-        const completedInit = localStorage.getItem('completedInit')
-        if (completedInit !== null && completedInit === 'true') {
-          return '/loadSaveData'
-        } else {
-          return '/opening'
-        }
+        return '/event'
       }
     }
   }
