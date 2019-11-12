390178d9e93f59176d12008bcb65cf1631f7279c
marco <28888207+Marco3jp@users.noreply.github.com>
2019-11-13 06:38:55 +0900

    各行の表示時にコンソールの下端に表示する処理を実装

--- components/consoleRow.vue
+++ components/consoleRow.vue
@@ -10,6 +10,11 @@
   export default {
     name: 'consoleRow',
     props: ['sentence', 'isCurrentRow'],
+    data: function() {
+      return {
+        unSubscribe: undefined
+      }
+    },
     computed: {
       row() {
         if (this.isCurrentRow && !this.$store.state.view.isEndSection) {
@@ -17,18 +22,31 @@
         } else {
           return this.sentence.text
         }
-      },
-      scrollBottom() {
-        return this.$store.state.console.doScrollBottom
       }
     },
-    watch: {
-      'scrollBottom': function(val) {
-        if (val && this.isCurrentRow) {
-          this.$refs.row.scrollIntoView(true)
+    // TODO: 一行目だけ問題が発生する可能性があるものの、よっぽど特殊な環境でなければプレイ上の問題はないので一旦修正を見送り。
+    // このIssueは https://github.com/Marco3jp/us-life/issues/6 で管理される。
+    mounted() {
+      this.unSubscribe = this.$store.subscribe((mutation) => {
+        console.log(this.isCurrentRow, mutation.type, this.$refs, this.sentence)
+        if (mutation.type === 'console/scrollBottom' && this.isCurrentRow && typeof this.$refs.row !== 'undefined') {
+          this.$refs.row.scrollIntoView()
           this.$store.commit('console/doneScrollBottom')
         }
+      })
+    },
+    watch: {
+      'isCurrentRow': function(val) {
+        if (!val) {
+          this.unSubscribe()
+        }
       }
+    },
+    /**
+     * 最後の行は最後までisCurrentRowであり、DOM上から消えても参照が死なずに動き続けるのを回避するため
+     */
+    beforeDestroy() {
+      this.unSubscribe()
     }
   }
 </script>
